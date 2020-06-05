d60dbdc1b11a7e89bfef1060b0545f9d4e7d51a6
Crysteier <crysteier12345@gmail.com>
2020-06-06 01:18:47 +0200

    urobene grafy hotove

--- moricz/forChart.js
+++ moricz/forChart.js
@@ -1,33 +1,138 @@
 $(document).ready(function () {
     $(document).on('input','#graph',function () {
-        $('#ch').toggle();
+        $('#chart1').toggle();
+        $('#chart2').toggle();
     });
-    var ctx = document.getElementById('ch');
+
+    var ctx = document.getElementById('chart1');
+    var ctx2 = document.getElementById('chart2');
+
+    function addData(chart, data,label) {
+        chart.data.labels.push(label);
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+        });
+        chart.update();
+    }
+
+    function addData2(chart, data1,data2,label) {
+        chart.data.labels.push(label);
+        chart.data.datasets[1].data.push(data1);
+        chart.data.datasets[0].data.push(data2);
+        chart.update();
+    }
+
+    var myChart2 = new Chart(ctx2,{
+        type: 'line',
+        data:{
+            labels: [],
+            datasets: [{
+                label: 'Dolne teleso',
+                data:[0],
+                id: 'jedna',
+                borderColor: "blue"
+            },{
+                label: 'Horne teleso',
+                data:[],
+                backgroundColor: "rgba(225,0,0,0.4)",
+                fill:false,
+                borderColor: "red",
+                id: 'dva'
+            }]},
+        options: {
+            responsive: false
+        }
+    });
+
     var myChart = new Chart(ctx, {
         type: 'line',
-        data:[],
+        data:{
+            datasets: [{
+                label: 'Tlmenie',
+                data:[]
+            }]},
         options: {
             responsive: false,
         }
     });
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     $('#btn').click(function (event) {
-        event.preventDefault();
+        var myChart2 = new Chart(ctx2,{
+            type: 'line',
+            data:{
+                labels: [],
+                datasets: [{
+                    label: 'Dolne teleso',
+                    borderWidth:1,
+                    pointBorderWidth: 0,
+                    pointRadius:0.5,
+                    data:[0],
+                    id: 'jedna',
+                    backgroundColor: "rgba(0,0,255,0.4)",
+                    fill: false,
+                    borderColor: "blue"
+                },{
+                    label: 'Horne teleso',
+                    data:[],
+                    backgroundColor: "rgba(225,0,0,0.4)",
+                    fill:false,
+                    borderColor: "red",
+                    id: 'dva'
+                }]},
+            options: {
+                responsive: false,
+
+            },
+        });
+
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data:{
+                datasets: [{
+                    label: 'Tlmenie',
+                    data:[]
+                }]},
+            options: {
+                responsive: false,
+            }
+        });
+         event.preventDefault();
+
         var data = {
             "action" : "tlmic",
-            "x1":0,
-            "x1d":0,
-            "x2":0,
-            "x2d":0,
-            "r":0.1,
-           // "key":key
+            "x1":parseInt($('#x1').val()),
+            "x1d":parseInt($('#x1d').val()),
+            "x2":parseInt($('#x2').val()),
+            "x2d":parseInt($('#x2d').val()),
+            "r":parseFloat($('#rSize').val()),
+            "key": "brutalny_api_kluc_123"
         };
+
         $.ajax({
             type: "POST",
             dataType: "json",
             data: data,
-            url:"http://147.175.121.210:8233/final_zadanie/ajax.php?",
-            complete: async function(datas){
-                console.log(datas);
+            url:"https://147.175.121.210:4497/skuska_spolu/moricz/ajax.php?",
+            //url:"https://147.175.121.210:8233/final_zadanie/ajax.php?",
+            complete: async function(dattas){
+                console.log(dattas);
+                for (i=0;i<dattas.responseJSON.length;i++){
+                    console.log("1 udaj: "+dattas.responseJSON[i][0]);
+                    console.log("2 udaj: "+dattas.responseJSON[i][1]);
+                    console.log("3 udaj: "+dattas.responseJSON[i][2]);
+                    console.log(" ");
+                    if (dattas.responseJSON[i][0] === undefined){
+                        break;
+                    }
+                    addData(myChart,parseFloat(dattas.responseJSON[i][1]),dattas.responseJSON[i][2]);
+                    addData2(myChart2,parseFloat(dattas.responseJSON[i][1]),parseFloat(dattas.responseJSON[i][0]),dattas.responseJSON[i][2]);
+
+                  //  await sleep(100);
+                }
             }
         })
     })
--- moricz/index.php
+++ moricz/index.php
@@ -5,20 +5,48 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3"></script>
+    <script src="https://cdn.jsdelivr.net/npm/hammerjs@2.0.8"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-zoom@0.7.7"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
     <title>Tlmenie kolesa</title>
+    <style>
+        .center{
+            display: flex;
+            justify-content: center;
+        }
+        canvas{
+            width: 50rem;
+            height: 400px;
+        }
+    </style>
 </head>
 <body>
-<canvas id="ch" width="200" height="200"></canvas>
+
+<div class="center">
+<canvas id="chart1" ></canvas>
+<canvas id="chart2"></canvas>
+</div>
 
 <form action="">
     <label for="graph">Graph toggle</label>
     <input type="checkbox" name="graph" id="graph" checked>
+
+    <label for="rSize">Väľkosť prekážky</label>
+    <input type="number" step="0.1" id="rSize" name="r" value="0">
+
     <label for="start">Start</label>
     <input type="submit" value="Start" id="btn">
 </form>
+<input type="hidden" value="0" id="x1">
+<input type="hidden" value="0" id="x1d">
+<input type="hidden" value="0" id="x2">
+<input type="hidden" value="0" id="x2d">
 <script src="forChart.js"></script>
 </body>
 </html>
